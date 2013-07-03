2da6aa3df285586fd380539a0245053770413a34
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-03 11:49:00 -0700

    finished models and migration tables, created db and migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+  has_many :comments
+  has_many :posts
+
+  validates :username, uniqueness: true
+
+  def password
+      @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+      @password = Password.create(new_password)
+      self.password_hash = @password
+  end
 end
--- db/migrate/20130703113338_create_posts.rb
+++ db/migrate/20130703113338_create_posts.rb
@@ -1,4 +1,12 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :url
+      t.string :discussion
+      t.references :user
+
+      t.timestamps
+    end
   end
 end
